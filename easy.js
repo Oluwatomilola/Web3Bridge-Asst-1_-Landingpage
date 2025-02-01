7ee633f2ac644a9696a57b6b7bd9a0aabe09dbca
Oluwatomilola <tommy4gold@gmail.com>
2025-02-01 15:28:09 +0100

    js added

---
+++ easy.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("nav ul li");
+    const requestButtons = document.querySelectorAll("button");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function() {
+            alert(`You clicked on ${this.textContent}`);
+        });
+    });
+
+    requestButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            alert("Request Invite button clicked");
+        });
+    });
+});
